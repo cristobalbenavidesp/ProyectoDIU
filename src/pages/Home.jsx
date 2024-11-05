9ef6c36226b25e40b4881b211fc4a16a67fe2a3a
AngeloIbadurmiendo <angelo.ibaceta@usm.cl>
2024-11-05 17:10:26 -0300

    Home listo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,34 @@ import LinkButton from "../components/ui/LinkButton";
 function Home() {
   const [userName, setUserName] = useState("Matías Godoy");
   return (
-    <div className="w-full h-full min-h-screen flex flex-col gap-5 items-center place-content-center ">
-      <h1 className="text-4xl mb-10 text-center">¡Bienvenido, {userName}!</h1>
-      <LinkButton type="primary" href="/perfil-fisico">
-        Mi estado físico
-      </LinkButton>
-      <LinkButton type="primary" href="/perfil-mental">
-        Mi salud mental
-      </LinkButton>
+    <div className="relative w-full h-full min-h-screen">
+      {/* Fondo con desenfoque */}
+      <div className="absolute inset-0 bg-[url('/background.webp')] bg-cover bg-center filter blur-sm -z-10"></div>
+      
+      {/* Contenido */}
+      <div className="w-full min-h-screen flex flex-col items-center justify-center text-white px-4 relative z-10">
+        <div className="p-8 text-center">
+        <h1 className="text-4xl font-bold mb-8 text-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.6)]">
+            ¡Bienvenido, {userName}!
+          </h1>
+          <div className="flex flex-col gap-4 w-full max-w-xs items-center">
+            <LinkButton
+              type="primary"
+              href="/perfil-fisico"
+              className="transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl bg-blue-600 py-3 px-6 rounded-full text-white bg-opacity-90 backdrop-blur-md"
+            >
+              Mi estado físico
+            </LinkButton>
+            <LinkButton
+              type="primary"
+              href="/perfil-mental"
+              className="transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl bg-blue-600 py-3 px-6 rounded-full text-white bg-opacity-90 backdrop-blur-md"
+            >
+              Mi salud mental
+            </LinkButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
