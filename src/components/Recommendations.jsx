93ff49d6da007dfd3e01bc155aadce62144406f5
Cristobal Benavides <cristobal.benavidesp@sansano.usm.cl>
2024-11-05 21:04:09 -0300

    fixed card design for recommendations

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -5,17 +5,28 @@ import Card from "./ui/Card";
 function Recommendations({ articles }) {
   return (
     <article className="py-5 w-full h-fit bg-cover bg-center rounded-lg  p-5">
-      <h1 className="text-xl mb-5 text-center text-white font-semibold ">Recomendaciones según tu estado</h1>
+      <h1 className="text-xl mb-5 text-center text-white font-semibold ">
+        Recomendaciones según tu estado
+      </h1>
       <ul className="flex flex-col gap-3 py-5">
         {articles.map((article, index) => (
           <li key={index} className="flex">
             <Card>
-              <div className="flex gap-3">
-                <img src={article.image} alt={article.title} className="w-20 h-20 object-cover rounded-md" />
+              <div className="flex flex-col gap-3 pb-2">
+                <img
+                  src={article.image}
+                  alt={article.title}
+                  className="w-full aspect-video object-cover rounded-md"
+                />
                 <div>
                   <h2 className="text-lg font-bold">{article.title}</h2>
                   <p className="text-sm">{article.description}</p>
-                  <a href={article.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm mt-2 block">
+                  <a
+                    href={article.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 text-sm mt-2 block"
+                  >
                     Leer más
                   </a>
                 </div>
